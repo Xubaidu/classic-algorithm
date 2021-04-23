2e1d402df1ac7db99f10f39310c9e0b5e743f4a7
ChenyangXu <48062706+Xubaidu@users.noreply.github.com>
2021-04-23 21:51:36 +0800

    correct some errors

--- Miller-Rabin/bigInteger.cpp
+++ Miller-Rabin/bigInteger.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cmp(string A, string B);
+/* 
+ * overload "+, -, *, /, %" by bigInteger
+ * bigInteger is represented by string
+ */
+bool operator < (const string &a, const string &b);
+bool operator <= (const string &a, const string &b);
+bool operator > (const string &a, const string &b);
+bool operator >= (const string &a, const string &b);
 string operator + (const string& a, const string& b);
 string operator - (const string& a, const string& b);
 string operator * (const string& a, const string& b);
@@ -12,36 +19,109 @@ int main()
 {
     freopen("in.txt", "r", stdin);
     string s1, s2, s3, s4, s5, s6, s7;
-    int f = 0;
-    while (cin >> s1 >> s2 >> s3 >> s4 >> s5 >> s6 >> s7) {
-        int f[5] = {0};
+    string x1, x2, x3, x4;
+    int flag = 0;
+    map<string, bool> mp;
+    mp["False"] = false;
+    mp["True"] = true;
+    int cnt = 0;
+    while (cin >> cnt >> s1 >> s2 >> s3 >> s4 >> s5 >> s6 >> s7 >> x1 >> x2 >> x3 >> x4) {
+        int f[11] = {0};
+        //cout << x5 << endl;
         if (s1 + s2 == s3) f[0] = 1;
         if (s1 - s2 == s4) f[1] = 1;
         if (s1 * s2 == s5) f[2] = 1;
         if (s1 / s2 == s6) f[3] = 1;
         if (s1 % s2 == s7) f[4] = 1;
-        for (int i = 0; i < 5; ++i) {
+        
+        if ((s1 < s2) == mp[x1]) f[5] = 1;
+        if ((s1 <= s2) == mp[x2]) f[6] = 1;
+        if ((s1 > s2) == mp[x3]) f[7] = 1;
+        if ((s1 >= s2) == mp[x4]) f[8] = 1; 
+        printf("%02d ", cnt++);
+        for (int i = 0; i < 9; ++i) {
             if (f[i] == 0) cout << "×";
             else cout << "√";
         }
         cout << endl;
     }
-    //puts(!f ? "AC" : "WA");
     return 0;
 }
 
-
-int cmp(string A, string B)
+bool operator < (const string &a, const string &b)
 {
-    if (A.length() < B.length()) return -1;
-    else if (A.length() > B.length()) return 1;
+    if (a[0] == '-' && b[0] != '-') return 1; // negative < positive
+    else if (a[0] != '-' && b[0] == '-') return 0; // positive > negative
     else {
-        for (int i = 0; i < A.length(); ++i) {
-            if (A[i] < B[i]) return -1;
-            else if (A[i] > B[i]) return 1;
+        int flag = (a[0] == '-') ? 0 : 1;
+        string A = (a[0] == '-') ? a.substr(1, a.length()) : a.substr(0, a.length());
+        string B = (b[0] == '-') ? b.substr(1, b.length()) : b.substr(0, b.length());
+        if (a.length() < b.length()) return flag;
+        else if (a.length() > b.length()) return flag ^ 1;
+        for (int i = 0; i < a.length(); ++i) {
+            if (a[i] < b[i]) return flag;
+            else if (a[i] > b[i]) return flag ^ 1;
+            else continue;
         }
-        return 0;
     }
+    return 0;
+}
+
+bool operator <= (const string &a, const string &b)
+{
+    if (a[0] == '-' && b[0] != '-') return 1; // negative <= positive
+    else if (a[0] != '-' && b[0] == '-') return 0; // positive >= negative
+    else {
+        int flag = (a[0] == '-') ? 0 : 1;
+        string A = (a[0] == '-') ? a.substr(1, a.length()) : a.substr(0, a.length());
+        string B = (b[0] == '-') ? b.substr(1, b.length()) : b.substr(0, b.length());
+        if (a.length() < b.length()) return flag;
+        else if (a.length() > b.length()) return flag ^ 1;
+        for (int i = 0; i < a.length(); ++i) {
+            if (a[i] < b[i]) return flag;
+            else if (a[i] > b[i]) return flag ^ 1;
+            else continue;
+        }
+    }
+    return 1;
+}
+
+bool operator > (const string &a, const string &b)
+{
+    if (a[0] == '-' && b[0] != '-') return 0; // negative < positive
+    else if (a[0] != '-' && b[0] == '-') return 1; // positive > negative
+    else {
+        int flag = (a[0] == '-') ? 1 : 0;
+        string A = (a[0] == '-') ? a.substr(1, a.length()) : a.substr(0, a.length());
+        string B = (b[0] == '-') ? b.substr(1, b.length()) : b.substr(0, b.length());
+        if (a.length() < b.length()) return flag;
+        else if (a.length() > b.length()) return flag ^ 1;
+        for (int i = 0; i < a.length(); ++i) {
+            if (a[i] < b[i]) return flag;
+            else if (a[i] > b[i]) return flag ^ 1;
+            else continue;
+        }
+    }
+    return 0;
+}
+
+bool operator >= (const string &a, const string &b)
+{
+    if (a[0] == '-' && b[0] != '-') return 0;
+    else if (a[0] != '-' && b[0] == '-') return 1;
+    else {
+        int flag = (a[0] == '-') ? 1 : 0;
+        string A = (a[0] == '-') ? a.substr(1, a.length()) : a.substr(0, a.length());
+        string B = (b[0] == '-') ? b.substr(1, b.length()) : b.substr(0, b.length());
+        if (a.length() < b.length()) return flag;
+        else if (a.length() > b.length()) return flag ^ 1;
+        for (int i = 0; i < a.length(); ++i) {
+            if (a[i] < b[i]) return flag;
+            else if (a[i] > b[i]) return flag ^ 1;
+            else continue;
+        }
+    }
+    return 1;
 }
 
 string operator + (const string& a, const string& b)
@@ -82,9 +162,10 @@ string operator - (const string& a, const string& b)
         B = b.substr(1, b.length());
         return B - A;
     }
-    else if (a[0] == '-' && b[0] != '-') { // -a - b = (-a) + (-b)
-        A = a;
+    else if (a[0] == '-' && b[0] != '-') { // -a - b = -(a + b)
+        A = a.substr(1, a.length());
         B = b;
+        //cout << A + B << endl;
         return (A + B).insert(0, "-");
     }
     else if (a[0] != '-' && b[0] == '-') { // a - (-b) = a + b
@@ -95,7 +176,7 @@ string operator - (const string& a, const string& b)
     else { // a - b
         A = a, B = b;
         int flag = 0;
-        if (cmp(A, B) == -1) swap(A, B), flag = 1;
+        if (A < B) swap(A, B), flag = 1;
         reverse(A.begin(), A.end());
         reverse(B.begin(), B.end());
         int L1 = A.length(), L2 = B.length();
@@ -153,14 +234,13 @@ string operator * (const string& a, const string& b)
     return C;
 }
 
-
 string operator / (const string& a, const string& b)
 {
     string A = a;
     string B = b;
     if (B == "0") return "false";
-    if (cmp(A, B) < 0) return "0";
-    else if (cmp(A, B) == 0) return "1";
+    if (A < B) return "0";
+    else if (A == B) return "1";
     else {
         int L1 = A.length(), L2 = B.length();
         string C = "";
@@ -169,19 +249,23 @@ string operator / (const string& a, const string& b)
             string x = string(1, A[i]);
             temp = temp + x;
             int cnt = 0;
-            while (cmp(temp, B) >= 0) temp = temp - B, cnt++;
+            while (temp >= B) {
+                temp = temp - B, cnt++;
+            }
             temp = temp * "10";
             C += cnt + '0';
         }
         int pos = 0;
         while (C[pos] == '0') pos++;
+        //cout << C << endl;
         return C.substr(pos, C.length() - pos + 1);
     }
 }
 
-
 string operator % (const string& a, const string& b)
 {
     if (b == "0") return "false";
-    return a - a / b * b;
+    string A = a;
+    while (A[0] == '-') A = A + b;
+    return A - A / b * b;
 }
